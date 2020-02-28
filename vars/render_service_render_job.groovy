784206f1f22ebee97b44a6a0bbb3480a811b9a09
Andrey Kravchenko <epozine@inbox.ru>
2020-02-28 17:31:08 +0300

    Copy from demo pipeline

--- vars/render_service_render_job.groovy
+++ vars/render_service_render_job.groovy
@@ -1,11 +1,11 @@
 def executeRender(osName, gpuName, Map options) {
-    currentBuild.result = 'SUCCESS'
-    
-    String tool = options['Tool'].split(':')[0].trim()
-    String version = options['Tool'].split(':')[1].trim()
-    String scene_name = options['Scene'].split('/')[-1].trim()
+	currentBuild.result = 'SUCCESS'
+
+	String tool = options['Tool'].split(':')[0].trim()
+	String version = options['Tool'].split(':')[1].trim()
+	String scene_name = options['sceneName']
 	String fail_reason = "Unknown"
-    
+
 	timeout(time: 65, unit: 'MINUTES') {
 		switch(osName) {
 			case 'Windows':
@@ -15,47 +15,79 @@ def executeRender(osName, gpuName, Map options) {
 						@echo off
 						del /q *
 						for /d %%x in (*) do @rd /s /q "%%x"
-					''' 
+					'''
+					// Download render service scripts
+					try {
+					    print("Check scripts")
+					    def exists = fileExists "..\\Scripts"
+					    if (exists){
+					        print("Pull from git to update")
+					        dir("..\\Scripts"){
+					        	bat """
+					        	git branch --set-upstream-to=origin/${options.scripts_branch} ${options.scripts_branch}
+					        	git pull
+					        	"""
+					        }
+					    } else {
+					        dir("..\\Scripts"){
+					    	    print("Downloading scripts")
+					    	    git url:"https://github.com/luxteam/render_service_scripts.git", branch: "${options.scripts_branch}"
+					    	}
+					    }
+					    dir("..\\Scripts"){
+					        	bat '''
+					        	pip3 install -r requirements.txt
+					        	'''
+					        }
+					} catch(e) {
+						print e
+						fail_reason = "Downloading scripts failed"
+					}
 					// download scene, check if it is already downloaded
 					try {
-						print(python3("${CIS_TOOLS}\\${options.cis_tools}\\send_render_status.py --django_ip \"${options.django_url}/\" --tool \"${tool}\" --status \"Downloading scene\" --id ${id}"))
+					    dir("..\\..\\RenderServiceStorage"){
+					        writeFile file:'test', text:'dir created'
+					    }
+						print(python3("..\\Scripts\\send_render_status.py --django_ip \"${options.django_url}/\" --tool \"${tool}\" --status \"Downloading scene\" --id ${id}"))
 						def exists = fileExists "..\\..\\RenderServiceStorage\\${scene_name}"
 						if (exists) {
 							print("Scene is copying from Render Service Storage on this PC")
 							bat """
-								copy "..\\..\\RenderServiceStorage\\"${options.sceneName}" "${options.sceneName}"
+								copy "..\\..\\RenderServiceStorage\\${scene_name}" "${scene_name}"
 							"""
 						} else {
-							bat """ 
-								wget --no-check-certificate "${options.Scene}"
-							"""
+							withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'renderServiceCredentials', usernameVariable: 'DJANGO_USER', passwordVariable: 'DJANGO_PASSWORD']]) {
+								bat """ 
+									curl -o "${scene_name}" -u %DJANGO_USER%:%DJANGO_PASSWORD% "${options.Scene}"
+								"""
+							}
 							bat """
-								copy "${options.sceneName}" "..\\..\\RenderServiceStorage"
+								copy "${scene_name}" "..\\..\\RenderServiceStorage"
 							"""
 						}
 					} catch(e) {
 						print e
-						fail_reason = "Downloading failed"
+						fail_reason = "Downloading scene failed"
 					}
-					
-					// unzip
-					try {
-						if ("${scene_name}".endsWith('.zip') || "${scene_name}".endsWith('.7z')) {
-							bat """
-								7z x "${options.sceneName}"
-							"""
-						}
-					} catch(e) {
+
+                     // Unpacking scene
+                    try{
+                        bat """
+							copy "..\\Scripts\\unpack.py" "."
+						"""
+                        python3("unpack.py")
+                    } catch(e) {
 						print e
-						fail_reason = "Incorrect zip file"
+						fail_reason = "Unpacking scene failed"
 					}
 					
+
 					switch(tool) {
-						case 'Blender':  
+						case 'Blender':
 							// copy necessary scripts for render
 							bat """
-								copy "${CIS_TOOLS}\\${options.cis_tools}\\blender_render.py" "."
-								copy "${CIS_TOOLS}\\${options.cis_tools}\\launch_blender.py" "."
+								copy "..\\Scripts\\blender_render.py" "."
+								copy "..\\Scripts\\launch_blender.py" "."
 							"""
 							// Launch render
 							try {
@@ -64,8 +96,8 @@ def executeRender(osName, gpuName, Map options) {
 								print e
 								// if status == failure then copy full path and send to slack
 								bat """
-									mkdir "..\\..\\RenderServiceStorage\\failed_${options.sceneName}_${id}_${currentBuild.number}"
-									copy "*" "..\\..\\RenderServiceStorage\\failed_${options.sceneName}_${id}_${currentBuild.number}"
+									mkdir "..\\..\\RenderServiceStorage\\failed_${scene_name}_${id}_${currentBuild.number}"
+									copy "*" "..\\..\\RenderServiceStorage\\failed_${scene_name}_${id}_${currentBuild.number}"
 								"""
 							}
 							break;
@@ -73,8 +105,8 @@ def executeRender(osName, gpuName, Map options) {
 						case 'Max':
 							// copy necessary scripts for render
 							bat """
-								copy "${CIS_TOOLS}\\${options.cis_tools}\\max_render.ms" "."
-								copy "${CIS_TOOLS}\\${options.cis_tools}\\launch_max.py" "."
+								copy "..\\Scripts\\max_render.ms" "."
+								copy "..\\Scripts\\launch_max.py" "."
 							"""
 							// Launch render
 							try {
@@ -83,17 +115,17 @@ def executeRender(osName, gpuName, Map options) {
 								print e
 								// if status == failure then copy full path and send to slack
 								bat """
-									mkdir "..\\..\\RenderServiceStorage\\failed_${options.sceneName}_${id}_${currentBuild.number}"
-									copy "*" "..\\..\\RenderServiceStorage\\failed_${options.sceneName}_${id}_${currentBuild.number}"
+									mkdir "..\\..\\RenderServiceStorage\\failed_${scene_name}_${id}_${currentBuild.number}"
+									copy "*" "..\\..\\RenderServiceStorage\\failed_${scene_name}_${id}_${currentBuild.number}"
 								"""
 							}
 							break;
 
 						case 'Maya':
-							// copy necessary scripts for render	
+							// copy necessary scripts for render
 							bat """
-								copy "${CIS_TOOLS}\\${options.cis_tools}\\maya_render.py" "."
-								copy "${CIS_TOOLS}\\${options.cis_tools}\\launch_maya.py" "."
+								copy "..\\Scripts\\maya_render.py" "."
+								copy "..\\Scripts\\launch_maya.py" "."
 							"""
 							// Launch render
 							try {
@@ -102,17 +134,17 @@ def executeRender(osName, gpuName, Map options) {
 								print e
 								// if status == failure then copy full path and send to slack
 								bat """
-									mkdir "..\\..\\RenderServiceStorage\\failed_${options.sceneName}_${id}_${currentBuild.number}"
-									copy "*" "..\\..\\RenderServiceStorage\\failed_${options.sceneName}_${id}_${currentBuild.number}"
+									mkdir "..\\..\\RenderServiceStorage\\failed_${scene_name}_${id}_${currentBuild.number}"
+									copy "*" "..\\..\\RenderServiceStorage\\failed_${scene_name}_${id}_${currentBuild.number}"
 								"""
 							}
 							break;
 
 						case 'Maya (Redshift)':
-							// copy necessary scripts for render	
+							// copy necessary scripts for render
 							bat """
-								copy "${CIS_TOOLS}\\${options.cis_tools}\\redshift_render.py" "."
-								copy "${CIS_TOOLS}\\${options.cis_tools}\\launch_maya_redshift.py" "."
+								copy "..\\Scripts\\redshift_render.py" "."
+								copy "..\\Scripts\\launch_maya_redshift.py" "."
 							"""
 							// Launch render
 							try {
@@ -121,17 +153,17 @@ def executeRender(osName, gpuName, Map options) {
 								print e
 								// if status == failure then copy full path and send to slack
 								bat """
-									mkdir "..\\..\\RenderServiceStorage\\failed_${options.sceneName}_${id}_${currentBuild.number}"
-									copy "*" "..\\..\\RenderServiceStorage\\failed_${options.sceneName}_${id}_${currentBuild.number}"
+									mkdir "..\\..\\RenderServiceStorage\\failed_${scene_name}_${id}_${currentBuild.number}"
+									copy "*" "..\\..\\RenderServiceStorage\\failed_${scene_name}_${id}_${currentBuild.number}"
 								"""
 							}
 							break;
 
 						case 'Core':
-							// copy necessary scripts for render	
+							// copy necessary scripts for render
 							bat """
-								copy "${CIS_TOOLS}\\${options.cis_tools}\\find_scene_core.py" "."
-								copy "${CIS_TOOLS}\\${options.cis_tools}\\launch_core_render.py" "."					
+								copy "..\\Scripts\\find_scene_core.py" "."
+								copy "..\\Scripts\\launch_core_render.py" "."
 							"""
 							// Launch render
 							try {
@@ -140,58 +172,58 @@ def executeRender(osName, gpuName, Map options) {
 								print e
 								// if status == failure then copy full path and send to slack
 								bat """
-									mkdir "..\\..\\RenderServiceStorage\\failed_${options.sceneName}_${id}_${currentBuild.number}"
-									copy "*" "..\\..\\RenderServiceStorage\\failed_${options.sceneName}_${id}_${currentBuild.number}"
+									mkdir "..\\..\\RenderServiceStorage\\failed_${scene_name}_${id}_${currentBuild.number}"
+									copy "*" "..\\..\\RenderServiceStorage\\failed_${scene_name}_${id}_${currentBuild.number}"
 								"""
 							}
 							break;
 
-						}   
+					}
 				} catch(e) {
 					currentBuild.result = 'FAILURE'
 					print e
-					print(python3("${CIS_TOOLS}\\${options.cis_tools}\\send_render_results.py --django_ip \"${options.django_url}/\" --build_number ${currentBuild.number} --status ${currentBuild.result} --fail_reason \"${fail_reason}\" --id ${id}"))
-				} 
-			  	break;
+					print(python3("..\\Scripts\\send_render_results.py --django_ip \"${options.django_url}/\" --build_number ${currentBuild.number} --status ${currentBuild.result} --fail_reason \"${fail_reason}\" --id ${id}"))
+				}
+				break;
 		}
-    }
+	}
 }
 
 def main(String PCs, Map options) {
 
 	timestamps {
-	    String PRJ_PATH="${options.PRJ_ROOT}/${options.PRJ_NAME}"
-	    String JOB_PATH="${PRJ_PATH}/${JOB_NAME}/Build-${BUILD_ID}".replace('%2F', '_')
-	    options['PRJ_PATH']="${PRJ_PATH}"
-	    options['JOB_PATH']="${JOB_PATH}"
+		String PRJ_PATH="${options.PRJ_ROOT}/${options.PRJ_NAME}"
+		String JOB_PATH="${PRJ_PATH}/${JOB_NAME}/Build-${BUILD_ID}".replace('%2F', '_')
+		options['PRJ_PATH']="${PRJ_PATH}"
+		options['JOB_PATH']="${JOB_PATH}"
 
-	    boolean PRODUCTION = true
+		boolean PRODUCTION = false
 
-	    if (PRODUCTION) {
-		options['django_url'] = "https://render.cis.luxoft.com/render/jenkins/"
-		options['plugin_storage'] = "https://render.cis.luxoft.com/media/plugins/"
-		options['cis_tools'] = "RenderServiceScriptsDebug"
-		options['jenkins_job'] = "RenderServiceRenderJob"
-	    } else {
-		options['django_url'] = "https://testrender.cis.luxoft.com/render/jenkins/"
-		options['plugin_storage'] = "https://testrender.cis.luxoft.com/media/plugins/"
-		options['cis_tools'] = "RenderServiceScripts"
-		options['jenkins_job'] = "RenderServiceRenderJob"
-	    }
+		if (PRODUCTION) {
+			options['django_url'] = "https://172.26.157.251:84/render/jenkins/"
+			options['plugin_storage'] = "https://172.26.157.251:84/media/plugins/"
+			options['scripts_branch'] = "master"
+			options['jenkins_job'] = "RenderServiceRenderJob"
+		} else {
+			options['django_url'] = "http://172.26.157.251:84/render/jenkins/"
+			options['plugin_storage'] = "http://172.26.157.251:84/media/plugins/"
+			options['scripts_branch'] = "master"
+			options['jenkins_job'] = "RenderServiceRenderJob"
+		}
 
 		def testTasks = [:]
 		List tokens = PCs.tokenize(':')
 		String osName = tokens.get(0)
 		String deviceName = tokens.get(1)
-		
+
 		String renderDevice = ""
-	    if (deviceName == "ANY") {
+		if (deviceName == "ANY") {
 			String tool = options['Tool'].split(':')[0].trim()
 			renderDevice = tool
-	    } else {
+		} else {
 			renderDevice = "gpu${deviceName}"
-	    }
-		
+		}
+
 		try {
 			echo "Scheduling Render ${osName}:${deviceName}"
 			testTasks["Render-${osName}-${deviceName}"] = {
@@ -207,47 +239,47 @@ def main(String PCs, Map options) {
 			}
 
 			parallel testTasks
-		    
-	    } catch(e) {
+
+		} catch(e) {
 			println(e.toString());
 			println(e.getMessage());
 			println(e.getStackTrace());
 			currentBuild.result = "FAILED"
 			print e
-	    } 
-	}    
-    
+		}
+	}
+
 }
-    
+
 def call(String PCs = '',
-    String id = '',
-    String Tool = '',
-    String Scene = '',  
-    String sceneName = '',
-    String Min_samples = '',
-    String Max_samples = '',
-    String Noise_threshold = '',
-    String startFrame = '',
-    String endFrame = '',
-    String Width = '',
-    String Height = ''
-    ) {
+		 String id = '',
+		 String Tool = '',
+		 String Scene = '',
+		 String sceneName = '',
+		 String Min_samples = '',
+		 String Max_samples = '',
+		 String Noise_threshold = '',
+		 String startFrame = '',
+		 String endFrame = '',
+		 String Width = '',
+		 String Height = ''
+) {
 	String PRJ_ROOT='RenderServiceRenderJob'
-	String PRJ_NAME='RenderServiceRenderJob'  
+	String PRJ_NAME='RenderServiceRenderJob'
 	main(PCs,[
-	    enableNotifications:false,
-	    PRJ_NAME:PRJ_NAME,
-	    PRJ_ROOT:PRJ_ROOT,
-	    id:id,
-	    Tool:Tool,
-	    Scene:Scene,
-	    sceneName:sceneName,
-	    Min_Samples:Min_Samples,
-	    Max_Samples:Max_Samples,
-	    Noise_threshold:Noise_threshold,
-	    startFrame:startFrame,
-	    endFrame:endFrame,
-	    Width:Width,
-	    Height:Height
-	    ])
-    }
+			enableNotifications:false,
+			PRJ_NAME:PRJ_NAME,
+			PRJ_ROOT:PRJ_ROOT,
+			id:id,
+			Tool:Tool,
+			Scene:Scene,
+			sceneName:sceneName,
+			Min_Samples:Min_Samples,
+			Max_Samples:Max_Samples,
+			Noise_threshold:Noise_threshold,
+			startFrame:startFrame,
+			endFrame:endFrame,
+			Width:Width,
+			Height:Height
+	])
+}
