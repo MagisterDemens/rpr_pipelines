a244af453c34a9c14051cf327693578234e17d55
GulDilin <zhenyagurin@gmail.com>
2020-03-02 19:22:04 +0300

    fix branch name

--- vars/render_service_render_job.groovy
+++ vars/render_service_render_job.groovy
@@ -179,11 +179,11 @@ def main(String PCs, Map options) {
 	    if (PRODUCTION) {
 		options['django_url'] = "http://172.26.157.251:84/render/jenkins/"
 		options['plugin_storage'] = "https://render.cis.luxoft.com/media/plugins/"
-		options['scripts_branch'] = "egurin/render_service"
+		options['scripts_branch'] = "egurin/unpacking"
 	    } else {
 		options['django_url'] = "https://testrender.cis.luxoft.com/render/jenkins/"
 		options['plugin_storage'] = "https://testrender.cis.luxoft.com/media/plugins/"
-		options['scripts_branch'] = "egurin/render_service"
+		options['scripts_branch'] = "egurin/unpacking"
 	    }
 
 		def testTasks = [:]
