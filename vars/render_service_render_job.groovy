270adb9239659af3da015a39f675806618de4310
GulDilin <zhenyagurin@gmail.com>
2020-03-02 21:52:17 +0300

    change url and branch

--- vars/render_service_render_job.groovy
+++ vars/render_service_render_job.groovy
@@ -177,13 +177,13 @@ def main(String PCs, Map options) {
 	    boolean PRODUCTION = true
 
 	    if (PRODUCTION) {
-		options['django_url'] = "http://172.26.157.251:84/render/jenkins/"
+		options['django_url'] = "https://render.cis.luxoft.com/render/jenkins/"
 		options['plugin_storage'] = "https://render.cis.luxoft.com/media/plugins/"
-		options['scripts_branch'] = "egurin/unpacking"
+		options['scripts_branch'] = "master"
 	    } else {
 		options['django_url'] = "https://testrender.cis.luxoft.com/render/jenkins/"
 		options['plugin_storage'] = "https://testrender.cis.luxoft.com/media/plugins/"
-		options['scripts_branch'] = "egurin/unpacking"
+		options['scripts_branch'] = "develop"
 	    }
 
 		def testTasks = [:]
