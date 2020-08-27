0eb66e62aaf58b1392bd3f4a7a60c24b57ce7e2e
Tarasenko Daniil <dtarasenko@luxoft.com>
2020-08-27 15:27:53 +0300

    add -NewStructure

--- vars/rpr_maxplugin_pipeline.groovy
+++ vars/rpr_maxplugin_pipeline.groovy
@@ -165,8 +165,7 @@ def executeTests(String osName, String asicName, Map options)
             throw e
         }
 
-        String REF_PATH_PROFILE="${options.REF_PATH}/${asicName}-${osName}"
-        String JOB_PATH_PROFILE="${options.JOB_PATH}/${asicName}-${osName}"
+        String REF_PATH_PROFILE="${options.REF_PATH}/${asicName}-${osName}-NewStructure"
 
         options.REF_PATH_PROFILE = REF_PATH_PROFILE
 
