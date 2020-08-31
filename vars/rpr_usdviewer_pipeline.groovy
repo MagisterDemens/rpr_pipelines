4d1949b108f444855c87512a1622aadff1a02878
Andrei Kravchenko <epozine@inbox.ru>
2020-08-31 17:36:22 +0300

    Update rprviewer build

--- vars/rpr_usdviewer_pipeline.groovy
+++ vars/rpr_usdviewer_pipeline.groovy
@@ -23,7 +23,9 @@ def executeBuildWindows(Map options)
 
             :: PySide
 
-            python RPRViewer\\deps\\PySide\\setup.py install --ignore-git --parallel=%NUMBER_OF_PROCESSORS% >> ${STAGE_NAME}.USDPixar.log 2>&1
+            cd RPRViewer\\deps\\PySide
+            python setup.py install --ignore-git --parallel=%NUMBER_OF_PROCESSORS% >> ..\\..\\..\\${STAGE_NAME}.USDPixar.log 2>&1
+            cd ..\\..\\..
 
             :: USD
 
@@ -31,11 +33,10 @@ def executeBuildWindows(Map options)
         
             :: HdRPRPlugin
 
-            cd HdRPRPlugin
-
             set PXR_DIR=%CD%\\USDPixar
             set INSTALL_PREFIX_DIR=%CD%\\RPRViewer\\inst
 
+            cd HdRPRPlugin
             cmake -B build -G "Visual Studio 15 2017 Win64" -Dpxr_DIR=%PXR_DIR% -DCMAKE_INSTALL_PREFIX=%INSTALL_PREFIX_DIR% ^
                 -DRPR_BUILD_AS_HOUDINI_PLUGIN=FALSE -DPXR_USE_PYTHON_3=ON >> ..\\${STAGE_NAME}.HdRPRPlugin.log 2>&1
             cmake --build build --config Release --target install >> ..\\${STAGE_NAME}.HdRPRPlugin.log 2>&1
